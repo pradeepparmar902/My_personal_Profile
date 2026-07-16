538c8a161d402238586274374c0f2ca15b5ac4d2
Pradeep Parmar <pradeep.parmar@wppmedia.com>
2026-07-17 00:53:12 +0530

    Updates

--- assets/Background3D-BVvePury.js
+++ assets/Background3D-BVvePury.js
@@ -1,4 +1,4 @@
-import{r as Gx,g as Wx,R as Xx,a as M1,b as dt,j as Ft}from"./index-CqB6xL-Y.js";/**
+import{r as Gx,g as Wx,R as Xx,a as M1,b as dt,j as Ft}from"./index-CZPS5hFV.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
