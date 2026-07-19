ffebe73a6100ece1a7c190a2c71a918066014ab8
Pradeep Parmar <pradeep.parmar@wppmedia.com>
2026-07-19 09:04:14 +0530

    Updates

--- assets/Background3D-BprEFWP_.js
+++ assets/Background3D-BprEFWP_.js
@@ -1,4 +1,4 @@
-import{r as Gx,g as Wx,R as Xx,a as M1,b as dt,j as Ft}from"./index-96v-laTH.js";/**
+import{r as Gx,g as Wx,R as Xx,a as M1,b as dt,j as Ft}from"./index-ly_hj4_a.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
